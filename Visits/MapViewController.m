00957ef593054accd8eb6082d523263af0132253
shoji-ishida <shoji.ishida@gmail.com>
2014-12-01 14:35:46 +0900

    added title to annotation. name from CLGeocoder is assigned.

--- Visits/MapViewController.m
+++ Visits/MapViewController.m
@@ -12,7 +12,6 @@
 
 @interface MapViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
 @end
 
 @implementation MapViewController
@@ -47,12 +46,15 @@
     //MKCoordinateSpan span = MKCoordinateSpanMake(0.01, 0.01);
     double accuracy = [self.accuracy doubleValue] * 1.5f;
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coord, accuracy, accuracy);
-    
+
     [self.mapView setRegion:region animated:YES];
+
+    
     CLLocation *location = [[CLLocation alloc] initWithLatitude:[self.latitude doubleValue] longitude:[self.longitude doubleValue]];
-    [MapViewController reverseGeocodeLocation:location];
     MKPointAnnotation* pin = [[MKPointAnnotation alloc] init];
     pin.coordinate = coord;
+    [MapViewController reverseGeocodeLocation:location annotation:pin];
+
     [self.mapView addAnnotation:pin];
 }
 
@@ -70,25 +72,37 @@
     return overlay;
 }
 
-/*
 - (MKAnnotationView *)mapView:(MKMapView *)mapView
             viewForAnnotation:(id )annotation
 {
+    MKPinAnnotationView* pinView = nil;
     static NSString* Identifier = @"PinAnnotationIdentifier";
-    MKPinAnnotationView* pinView = (MKPinAnnotationView *)[mapView
-                                                           dequeueReusableAnnotationViewWithIdentifier:Identifier];
-    if (pinView == nil) {
-        pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+    
+    // ignore if annotation is for user current location
+    if(annotation != mapView.userLocation) {
+        pinView = (MKPinAnnotationView *)[mapView
+                  dequeueReusableAnnotationViewWithIdentifier:Identifier];
+        if (pinView == nil) {
+            pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
                                                   reuseIdentifier:Identifier];
-        pinView.animatesDrop = YES;
-        return pinView;
+            pinView.animatesDrop = YES;
+            pinView.canShowCallout = YES;
+            return pinView;
+        }
+        pinView.annotation = annotation;
     }
-    pinView.annotation = annotation;
     return pinView;
+}
+
+/*
+- (void)mapView:(MKMapView *)mapView
+didSelectAnnotationView:(MKAnnotationView *)view
+{
+    NSLog(@"disSelectAnnotationView");
 }
 */
 
-+ (void)reverseGeocodeLocation:(CLLocation *)location
++ (void)reverseGeocodeLocation:(CLLocation *)location annotation:(MKPointAnnotation *)pin
 {
     CLGeocoder *geocoder = [[CLGeocoder alloc] init];
     [geocoder reverseGeocodeLocation:location completionHandler:
@@ -96,7 +110,16 @@
          if ([placemarks count] > 0) {
              
              CLPlacemark *placemark = (CLPlacemark *)[placemarks lastObject];
-             NSLog(@"%@", placemark.areasOfInterest);
+             [pin setTitle:placemark.name];
+             NSLog(@"%@", placemark.name);
+             NSLog(@"%@", placemark.country);
+             NSLog(@"%@", placemark.administrativeArea);
+             NSLog(@"%@", placemark.subAdministrativeArea);
+             NSLog(@"%@", placemark.locality);
+             NSLog(@"%@", placemark.subLocality);
+             NSLog(@"%@", placemark.thoroughfare);
+             NSLog(@"%@", placemark.subThoroughfare);
+             NSLog(@"%@", placemark.region);
         }
      }];
 }
